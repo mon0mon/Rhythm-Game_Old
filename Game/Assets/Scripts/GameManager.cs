ce0967b63c2f76dbe6d90222528482e6770b5f4c
mon0mon <51530417+mon0mon@users.noreply.github.com>
2020-07-07 16:58:15 +0900

    07-07_16:57
    - 노트 이중처리(미스 판정 이후 히트 판정) 해결
    - 대략적인 리듬게임 예시

---
+++ Game/Assets/Scripts/GameManager.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public AudioSource theMusic;
+    public bool startPlaying;
+    public BeatScroller theBS;
+    private static GameManager instance = null;
+
+    private void Start()
+    {
+        Initialize();
+    }
+    
+    // 싱글톤 구성
+    public static GameManager Instance
+    {
+        get { return instance; }
+    }
+    
+    private void Initialize()
+    {
+        // 싱글톤 구성
+        if (instance)
+        {
+            DestroyImmediate(gameObject);
+            return;
+        }
+
+        instance = this;
+        DontDestroyOnLoad(gameObject);
+        
+        QualitySettings.vSyncCount = 0;
+        Application.targetFrameRate = 60;
+    }
+
+    private void Update()
+    {
+        if (!startPlaying)
+        {
+            if (Input.anyKeyDown)
+            {
+                startPlaying = true;
+                theBS.hasStarted = true;
+                
+                theMusic.Play();
+            }
+        }
+    }
+
+    public void NoteHit()
+    {
+        Debug.Log("Hit On Time");
+    }
+
+    public void NoteMissed()
+    {
+        Debug.Log("Missed Note");
+    }
+}
---
+++ Game/Assets/Scripts/NoteObject.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NoteObject : MonoBehaviour
+{
+    private GameManager _gameManager;
+    
+    private bool isDeleted = false;
+    
+    public bool canBePressed;
+    public KeyCode KeyToPress;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        _gameManager = GameManager.Instance;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyToPress))
+        {
+            if (canBePressed)
+            {
+                DestroyImmediate(gameObject);
+                GameManager.Instance.NoteHit();
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Activator")
+        {
+            canBePressed = true;
+        } else if (other.CompareTag("DeadZone"))
+        {
+            Debug.Log("Destroied");
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Activator") && !isDeleted)
+        {
+            _gameManager.NoteMissed();
+            canBePressed = false;
+            isDeleted = true;
+        }
+    }
+}
